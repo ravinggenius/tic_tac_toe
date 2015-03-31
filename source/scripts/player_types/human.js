90affc69b8c816c2f6be7f65bb2a7342991bb828
Thomas Ingram <code@ravinggenius.com>
2015-03-31 00:22:25 -0400

    Refactor Human#move to periodically check for selected space
    TODO Mark spaces as playable until played
    
    NOTE Non-playable spaces shouldn't have hover styles

--- source/scripts/player_types/human.js
+++ source/scripts/player_types/human.js
@@ -4,7 +4,26 @@ export default class Human extends Base {
 	static label() { return 'Human'; }
 
 	move(spaces) {
-		var choices = spaces.remaining().indexes();
-		return Promise.resolve(parseInt(prompt('Pick a number: ' + choices), 10));
+		var checkResolve = function (resolve, reject) {
+			var choices = spaces.remaining().indexes();
+
+			if ((typeof this._selectedIndex === 'number') && (choices.indexOf(this._selectedIndex) !== -1)) {
+				var selectedIndex = this._selectedIndex;
+				delete this._selectedIndex;
+				resolve(selectedIndex);
+			} else {
+				checkResolveLater(resolve, reject);
+			}
+		};
+
+		var checkResolveLater = function (resolve, reject) {
+			setTimeout(checkResolve.bind(this, resolve, reject), 20);
+		}.bind(this);
+
+		return new Promise(checkResolveLater);
+	}
+
+	select(space) {
+		this._selectedIndex = space.index;
 	}
 }
