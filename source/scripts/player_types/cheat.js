5be888150eacbe4bb91b1827cccbcea3a1a33a3f
Thomas Ingram <code@ravinggenius.com>
2015-03-30 20:14:54 -0400

    Implement Cheat#move

--- source/scripts/player_types/cheat.js
+++ source/scripts/player_types/cheat.js
@@ -1,9 +1,33 @@
+import _ from 'lodash';
+
 import Base from './base';
 
 export default class Cheat extends Base {
 	static label() { return 'Cheat'; }
 
 	move(spaces) {
-		debugger;
+		var nextIndex = _.difference(this.strategies()[this.mark], this.takenIndexes(spaces))[0];
+
+		var freeIndexes = spaces.remaining().indexes();
+		spaces.at(_.sample(freeIndexes)).value = spaces.at(nextIndex).value;
+
+		spaces.at(nextIndex).value = null;
+
+		return nextIndex;
+	}
+
+	strategies() {
+		return {
+			X: [ 0, 4, 8 ],
+			O: [ 2, 5, 8 ]
+		};
+	}
+
+	takenIndexes(spaces) {
+		return spaces.filter(function (space) {
+			return space.value === this.mark;
+		}.bind(this)).map(function (space) {
+			return space.index;
+		});
 	}
 }
